4a136d3711520866f87a0ae8185e64d15573907f
Sunil <sunilleo@gmail.com>
2020-08-24 00:18:08 -0500

    initial check-in

---
+++ src/main/java/com/spring/security/oauth2/authorization/SpringOauth2AuthorizationApplication.java
@@ -0,0 +1,14 @@
+package com.spring.security.oauth2.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2AuthorizationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringOauth2AuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/security/oauth2/authorization/SpringOauth2AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.oauth2.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
